85c01f59a38275ee4391dc8b561232b13de00bd1
Jose Luis Fonseca <jose@ditecnologia.com>
2015-10-07 15:52:30 -0500

    couple of traits

---
+++ src/Traits/DispatchesCommands.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hechoenlaravel\JarvisFoundation\Traits;
+
+
+/**
+ * Trait DispatchesCommands
+ * Use this trait to dispatch the commands for the Laravel Tactician Command Bus.
+ * @author Jose Fonseca <jose@ditecnologia.com>
+ * @package Hechoenlaravel\JarvisFoundation\Traits
+ */
+trait DispatchesCommands {
+
+    /**
+     * Dispatch the Command
+     * @param string $command Full namespace class name or bind from the Laravel Service container for the command
+     * @param string $handler Full namespace class name or bind from the Laravel Service container form the command handler
+     * @param array $input
+     * @param array $middleware
+     * @return mixed
+     */
+    public function execute($command, $handler, array $input = [], array $middleware = [])
+    {
+        $bus = app('Joselfonseca\LaravelTactician\CommandBusInterface');
+        $bus->addHandler($command, $handler);
+        return $bus->dispatch($command, $input, $middleware);
+    }
+
+}
---
+++ src/Traits/EntityManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Hechoenlaravel\JarvisFoundation\Traits;
+
+
+/**
+ * Trait EntityManager
+ * Use it to Manage Entities and fields in your classes
+ * @author Jose Luis Fonseca <jose@ditecnologia.com>
+ * @package Hechoenlaravel\JarvisFoundation\Traits
+ */
+trait EntityManager {
+
+    use DispatchesCommands;
+
+    /**
+     * Generate an entity
+     * @param array $data
+     * @return mixed
+     */
+    public function generateEntity(array $data = [])
+    {
+        return $this->execute('Hechoenlaravel\JarvisFoundation\EntityGenerator\EntityGeneratorCommand',
+            'Hechoenlaravel\JarvisFoundation\EntityGenerator\Handler\EntityGeneratorHandler', $data, [
+                'Hechoenlaravel\JarvisFoundation\EntityGenerator\Middleware\EntityGeneratorValidator',
+                'Hechoenlaravel\JarvisFoundation\EntityGenerator\Middleware\SetPrefixAndTableName'
+            ]);
+    }
+
+    /**
+     * Generate a field for an entity
+     * @param array $data
+     * @return mixed
+     */
+    public function generateField(array $data = [])
+    {
+        return $this->execute('Hechoenlaravel\JarvisFoundation\FieldGenerator\FieldGeneratorCommand',
+            'Hechoenlaravel\JarvisFoundation\FieldGenerator\Handler\FieldGeneratorHandler', $data, [
+                'Hechoenlaravel\JarvisFoundation\FieldGenerator\Middleware\FieldGeneratorValidator',
+                'Hechoenlaravel\JarvisFoundation\FieldGenerator\Middleware\FieldTypeValidator',
+            ]);
+    }
+
+}
