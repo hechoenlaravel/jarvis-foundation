2dffbe2c4def7e5bfd47f21383ddc5f2e2727a30
Jose Luis Fonseca <jose@ditecnologia.com>
2016-01-14 22:58:41 -0500

    doc blocks

--- src/Traits/FlowManager.php
+++ src/Traits/FlowManager.php
@@ -23,45 +23,83 @@ use Hechoenlaravel\JarvisFoundation\Flows\Handler\CreateStepCommandHandler;
 use Hechoenlaravel\JarvisFoundation\Flows\Handler\CreateTransitionCommandHandler;
 use Hechoenlaravel\JarvisFoundation\Flows\Transition;
 
+/**
+ * Class FlowManager
+ * @package Hechoenlaravel\JarvisFoundation\Traits
+ */
 trait FlowManager
 {
     use DispatchesCommands;
 
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public function createFlow(array $data)
     {
         return $this->execute(CreateFlowCommand::class, CreateFlowCommandHandler::class, $data);
     }
 
+    /**
+     * @param Flow $flow
+     * @param array $data
+     * @return mixed
+     */
     public function updateFlow(Flow $flow, array $data)
     {
         return $this->execute(EditFlowCommand::class, EditFlowCommandHandler::class, array_merge(['flow' => $flow], $data));
     }
 
+    /**
+     * @param Flow $flow
+     * @return mixed
+     */
     public function deleteFlow(Flow $flow)
     {
         return $this->execute(DeleteFlowCommand::class, DeleteFlowCommandHandler::class, ['flow' => $flow]);
     }
 
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public function createStep(array $data)
     {
         return $this->execute(CreateStepCommand::class, CreateStepCommandHandler::class, $data, [SetStepOrder::class]);
     }
 
+    /**
+     * @param Step $step
+     * @param array $data
+     * @return mixed
+     */
     public function updateStep(Step $step, array $data)
     {
         return $this->execute(EditStepCommand::class, EditStepCommandHandler::class, array_merge(['step' => $step], $data));
     }
 
+    /**
+     * @param Step $step
+     * @return mixed
+     */
     public function deleteStep(Step $step)
     {
         return $this->execute(DeleteStepCommand::class, DeleteStepCommandHandler::class, ['step' => $step]);
     }
 
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public function createTransition(array $data)
     {
         return $this->execute(CreateTransitionCommand::class, CreateTransitionCommandHandler::class, $data, []);
     }
 
+    /**
+     * @param Transition $transition
+     * @return mixed
+     */
     public function deleteTransition(Transition $transition)
     {
         return $this->execute(DeleteTransitionCommand::class, DeleteTransitionCommandHandler::class, ['transition' => $transition], []);
