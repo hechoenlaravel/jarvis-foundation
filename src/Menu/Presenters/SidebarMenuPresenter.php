813169e4e2fefee8a35a1f71afa6b02ebf57985d
Jose Luis Fonseca <jose@ditecnologia.com>
2016-02-05 17:51:53 -0500

    Fix Presenter for multi dropdown menus

--- src/Menu/Presenters/SidebarMenuPresenter.php
+++ src/Menu/Presenters/SidebarMenuPresenter.php
@@ -39,4 +39,21 @@ class SidebarMenuPresenter extends NavbarPresenter
     {
         return '<li'.$this->getActiveState($item).'><a href="'.$item->getUrl().'" '.$item->getAttributes().'>'.$item->getIcon().' <span>'.$item->title.'</span></a></li>'.PHP_EOL;
     }
+
+    /**
+     * {@inheritdoc }.
+     */
+    public function getMultiLevelDropdownWrapper($item)
+    {
+        return '<li class="'.$this->getActiveStateOnChild($item, ' active').'">
+		          <a href="#">
+					'.$item->getIcon().' '.$item->title.'
+			      	<i class="fa fa-angle-left pull-right"></i>
+			      </a>
+			      <ul class="treeview-menu">
+			      	'.$this->getChildMenuItems($item).'
+			      </ul>
+		      	</li>'
+        .PHP_EOL;
+    }
 }
