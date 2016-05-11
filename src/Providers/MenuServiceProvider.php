0409884766874590e0eed64b3d0818a3e3ae2699
Jose Luis Fonseca <jose@ditecnologia.com>
2016-05-10 23:31:12 -0500

    Remove Config Menu

--- src/Providers/MenuServiceProvider.php
+++ src/Providers/MenuServiceProvider.php
@@ -20,14 +20,6 @@ class MenuServiceProvider extends ServiceProvider
                 $menu->setPresenter('Hechoenlaravel\JarvisFoundation\Menu\Presenters\SidebarMenuPresenter');
             });
         }
-        if (!MenuPing::instance('config')) {
-            MenuPing::create('config', function ($menu) {
-                $menu->dropdown('Configuración', function ($sub) {
-
-                }, ['icon' => 'fa fa-cogs']);
-                $menu->setPresenter('Hechoenlaravel\JarvisFoundation\Menu\Presenters\SidebarMenuPresenter');
-            });
-        }
         if (!MenuPing::instance('topnav')) {
             MenuPing::create('topnav', function ($menu) {
                 $menu->enableOrdering();
