9e41f9d24ad4e75672fa8b7ddd0bb98ae651f88a
Jose Luis Fonseca <jose@ditecnologia.com>
2017-10-21 21:44:30 -0500

    dock blocks

--- src/Flows/UI/FlowForm.php
+++ src/Flows/UI/FlowForm.php
@@ -5,32 +5,61 @@ namespace Hechoenlaravel\JarvisFoundation\Flows\UI;
 use JavaScript;
 use Hechoenlaravel\JarvisFoundation\Flows\Flow;
 
+/**
+ * Class FlowForm
+ *
+ * @package Hechoenlaravel\JarvisFoundation\Flows\UI
+ */
 class FlowForm
 {
+    /**
+     * @var null
+     */
     public $flow = null;
 
+    /**
+     * @var
+     */
     public $module;
 
+    /**
+     * @var
+     */
     public $returnBaseUrl;
 
+    /**
+     * @param $module
+     * @return $this
+     */
     public function setModule($module)
     {
         $this->module = $module;
         return $this;
     }
 
+    /**
+     * @param $url
+     * @return $this
+     */
     public function setReturnBaseUrl($url)
     {
         $this->returnBaseUrl = $url;
         return $this;
     }
 
+    /**
+     * @param $id
+     * @return $this
+     */
     public function setFlow($id)
     {
         $this->flow = Flow::findOrFail($id);
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function render()
     {
         JavaScript::put([
